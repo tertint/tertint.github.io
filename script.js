535915f9b603a0fe5c7aa60240cffb9689c28a4c
tertint <32640244+tertint@users.noreply.github.com>
2018-12-20 15:05:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-var targets = [                          //Place target URL here
+// var targets = [
+//'example',
+//];
 
+//     let iTarget;
 
-
-			'http://www.aif.ru',
-			'http://www.aif.ru/culture/person/prokaza_ne_udalas_kto_takoy_haski_i_chem_on_ne_nravitsya_policii',
-			'http://www.aif.ru/politics/world/meksika_napravila_notu_ssha_iz-za_incidenta_s_migrantami_na_granice',
-			'http://www.aif.ru/politics/russia/fsb_rasskazala_kak_razvivalis_sobytiya_v_rayone_kerchenskogo_proliva',
-			'https://www.vesti.ru/',
-			'https://www.mk.ru/social/2018/12/05/yandeks-i-gazprommedia-zaklyuchili-mirovoe-soglashenie.html',
-			'https://www.mk.ru/politics/2018/12/05/sud-kieva-zavershil-rassmotrenie-dela-yanukovicha.html',
-
-
-
-
-    ];
-
-    var iTarget;
-
-    function nextTarget(){
-        window.open( targets[iTarget], 'target' );
-        if( ++iTarget >= targets.length ) {
-            iTarget = 0;
-        }
-    }
-        iTarget = 0;
-        nextTarget();
-        setInterval( nextTarget, 60000 );   //time interval here 30000 = 30 Sec
+//     function nextTarget(){
+//         window.open( targets[iTarget], 'target' );
+//         if( ++iTarget >= targets.length ) {
+//             iTarget = 0;
+//         }
+//     }
+//         iTarget = 0;
+//         nextTarget();
+//         setInterval( nextTarget, 60000 );   //time interval here 30000 = 30 Sec
